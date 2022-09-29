cae8a55454aff7ef8bd78ca4906d77d26fc38a06
Brad Garropy <bgarropy@atlassian.com>
2022-09-29 09:35:17 -0500

    fix type issue.

--- app/routes/api/hello.tsx
+++ app/routes/api/hello.tsx
@@ -1,7 +1,7 @@
 import type {LoaderFunction} from "@remix-run/node"
 import {json} from "@remix-run/node"
 
-const loader: LoaderFunction = (): Promise<void> => {
+const loader: LoaderFunction = () => {
     return json({message: "world"}, {status: 200})
 }
 

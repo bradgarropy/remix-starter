34991293c1a66bc9cf058fa6c31c10c88aac5e02
Brad Garropy <bgarropy@atlassian.com>
2022-09-29 09:31:20 -0500

    Add typecheck script.

--- app/routes/api/hello.tsx
+++ app/routes/api/hello.tsx
@@ -1,7 +1,7 @@
 import type {LoaderFunction} from "@remix-run/node"
 import {json} from "@remix-run/node"
 
-const loader: LoaderFunction = () => {
+const loader: LoaderFunction = (): Promise<void> => {
     return json({message: "world"}, {status: 200})
 }
 

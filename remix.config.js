9810a5c207c3cda8a26339f8a1b2a7cf8db99a37
Brad Garropy <bgarropy@atlassian.com>
2023-03-01 14:36:20 -0600

    use builtin tailwind support.

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 
 const config = {
+    future: {
+        unstable_tailwind: true,
+    },
     ignoredRouteFiles: ["**/.*"],
     server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
     serverBuildPath: "api/index.js",

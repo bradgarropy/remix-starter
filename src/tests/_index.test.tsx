062dae8d5d1881337cce3b254a599504aab628da
Brad Garropy <bradgarropy@gmail.com>
2023-12-01 15:12:38 -0600

    fix home page test.

--- src/tests/_index.test.tsx
+++ src/tests/_index.test.tsx
@@ -1,6 +1,6 @@
 import {render, screen} from "@testing-library/react"
 
-import IndexRoute, {meta} from "~/routes/index"
+import IndexRoute, {meta} from "~/routes/_index"
 import {metaArgs} from "~/utils/testUtils"
 
 test("renders", () => {

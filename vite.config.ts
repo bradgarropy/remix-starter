d22145d1c5e96545daf8729842c7ba0a97dff018
Brad Garropy <bradgarropy@gmail.com>
2024-04-05 14:21:07 -0500

    combine vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {vitePlugin as remix} from "@remix-run/dev"
 import {installGlobals} from "@remix-run/node"
+import react from "@vitejs/plugin-react"
 import {defineConfig} from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
@@ -8,16 +9,34 @@ installGlobals()
 const config = defineConfig({
     plugins: [
         tsconfigPaths(),
-        remix({
-            appDirectory: "src",
-            ignoredRouteFiles: ["**/.*"],
-            future: {},
-            serverModuleFormat: "esm",
-        }),
+        process.env.VITEST
+            ? react()
+            : remix({
+                  appDirectory: "src",
+                  ignoredRouteFiles: ["**/.*"],
+                  future: {},
+                  serverModuleFormat: "esm",
+              }),
     ],
     server: {
         open: true,
         port: 3000,
+    },
+    test: {
+        clearMocks: true,
+        coverage: {
+            all: false,
+            clean: true,
+            enabled: true,
+            provider: "v8",
+            reporter: ["text", "lcov"],
+            reportOnFailure: false,
+        },
+        environment: "jsdom",
+        globals: false,
+        passWithNoTests: true,
+        setupFiles: "src/tests/setup.ts",
+        watch: false,
     },
 })
 

9c092a36b3f05399dd9c62fcbebe56e0c2e2d58a
Brad Garropy <bradgarropy@gmail.com>
2024-10-08 13:48:44 -0500

    configure source maps.

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,57 @@
 import {vitePlugin as remix} from "@remix-run/dev"
 import {installGlobals} from "@remix-run/node"
+import {sentryVitePlugin as sentry} from "@sentry/vite-plugin"
 import react from "@vitejs/plugin-react"
-import {defineConfig} from "vite"
+import {defineConfig, loadEnv} from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 installGlobals()
 
-const config = defineConfig({
-    plugins: [
-        tsconfigPaths(),
-        process.env.VITEST
-            ? react()
-            : remix({
-                  appDirectory: "src",
-                  ignoredRouteFiles: ["**/.*"],
-                  future: {},
-                  serverModuleFormat: "esm",
-              }),
-    ],
-    server: {
-        open: true,
-        port: 3000,
-    },
-    test: {
-        clearMocks: true,
-        coverage: {
-            all: false,
-            clean: true,
-            enabled: true,
-            provider: "v8",
-            reporter: ["text", "lcov"],
-            reportOnFailure: false,
+const config = defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), "")
+
+    return {
+        build: {
+            sourcemap: true,
         },
-        environment: "jsdom",
-        globals: false,
-        passWithNoTests: true,
-        setupFiles: "src/tests/setup.ts",
-        watch: false,
-    },
+        plugins: [
+            tsconfigPaths(),
+            env.VITEST
+                ? react()
+                : remix({
+                      appDirectory: "src",
+                      ignoredRouteFiles: ["**/.*"],
+                      future: {},
+                      serverModuleFormat: "esm",
+                  }),
+            sentry({
+                authToken: env.SENTRY_AUTH_TOKEN,
+                org: env.SENTRY_ORG,
+                project: env.SENTRY_PROJECT,
+                telemetry: false,
+            }),
+        ],
+        server: {
+            open: true,
+            port: 3000,
+        },
+        test: {
+            clearMocks: true,
+            coverage: {
+                all: false,
+                clean: true,
+                enabled: true,
+                provider: "v8",
+                reporter: ["text", "lcov"],
+                reportOnFailure: false,
+            },
+            environment: "jsdom",
+            globals: false,
+            passWithNoTests: true,
+            setupFiles: "src/tests/setup.ts",
+            watch: false,
+        },
+    }
 })
 
 export default config

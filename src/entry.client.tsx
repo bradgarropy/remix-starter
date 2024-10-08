c06e37bcce71dcd655b68fcb611a2a976161cad2
Brad Garropy <bradgarropy@gmail.com>
2024-10-08 12:55:47 -0500

    initialize sentry on client.

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -1,6 +1,12 @@
 import {RemixBrowser} from "@remix-run/react"
+import * as Sentry from "@sentry/remix"
 import {startTransition, StrictMode} from "react"
 import {hydrateRoot} from "react-dom/client"
+
+Sentry.init({
+    dsn: "https://bd22d6af8d5391631deea346947d86d0@o1037846.ingest.us.sentry.io/4508053968060416",
+    integrations: [],
+})
 
 startTransition(() => {
     hydrateRoot(

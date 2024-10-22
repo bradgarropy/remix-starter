a6de06f3fbc1bd6f53c5f1a1e935ccd29fad89d1
Brad Garropy <bradgarropy@gmail.com>
2024-10-22 10:42:18 -0500

    configure eslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,31 @@
-{
-    "extends": [
-        "@remix-run/eslint-config",
-        "@remix-run/eslint-config/node",
-        "@bradgarropy/eslint-config",
-        "@bradgarropy/eslint-config-react",
-        "@bradgarropy/eslint-config-typescript"
-    ]
-}
+import bgConfig from "@bradgarropy/eslint-config"
+import bgReactConfig from "@bradgarropy/eslint-config-react"
+import bgTypescriptConfig from "@bradgarropy/eslint-config-typescript"
+
+const config = [
+    ...bgConfig,
+    ...bgReactConfig,
+    ...bgTypescriptConfig,
+    {
+        ignores: [
+            // editor
+            ".vscode/**",
+            // dependencies
+            "node_modules/**",
+            // build
+            ".cache/**",
+            "build/**",
+            "public/build/**",
+            // test
+            "coverage/**",
+            "test-results/**",
+            "playwright-report/**",
+            // secrets
+            ".env*",
+            // os
+            ".DS_Store",
+        ],
+    },
+]
+
+export default config

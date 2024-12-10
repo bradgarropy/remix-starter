f4bfa9fd07960826876e393ca739bd0dcce09549
Brad Garropy <bradgarropy@gmail.com>
2024-12-10 16:02:38 -0600

    define routes.

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,13 @@
-import {flatRoutes} from "@remix-run/fs-routes"
+import type {RouteConfig} from "@remix-run/route-config"
+import {index, route} from "@remix-run/route-config"
 
-export default flatRoutes()
+const routes: RouteConfig = [
+    index("./routes/_index.tsx"),
+    route("about", "./routes/about.tsx"),
+    route("api/hello", "./routes/api/hello.tsx"),
+    route("sentry/frontend", "./routes/sentryFrontend.tsx"),
+    route("sentry/loader", "./routes/sentryLoader.tsx"),
+    route("sentry/action", "./routes/sentryAction.tsx"),
+]
+
+export default routes

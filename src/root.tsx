3eda621b3d1fd18387a9f206aaa147d1824a5344
Brad Garropy <bradgarropy@gmail.com>
2025-01-15 22:16:08 -0600

    define root error boundary.

--- src/root.tsx
+++ src/root.tsx
@@ -2,8 +2,9 @@ import type {LinksFunction, MetaFunction} from "@remix-run/node"
 import {Links, Meta, Outlet, Scripts, ScrollRestoration} from "@remix-run/react"
 import {withSentry} from "@sentry/remix"
 
-import Footer from "~/components/Footer/Footer"
-import Header from "~/components/Header/Header"
+import Error from "~/components/ErrorBoundary"
+import Footer from "~/components/Footer"
+import Header from "~/components/Header"
 import tailwindStyles from "~/styles/tailwind.css?url"
 
 export const meta: MetaFunction = () => [
@@ -47,6 +48,28 @@ const App = () => {
                     <Footer />
                 </div>
 
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    )
+}
+
+export const ErrorBoundary = () => {
+    return (
+        <html lang="en">
+            <head>
+                <meta
+                    httpEquiv="Content-Type"
+                    content="text/html;charset=utf-8"
+                />
+
+                <Meta />
+                <Links />
+            </head>
+
+            <body className="bg-white text-black">
+                <Error />
                 <ScrollRestoration />
                 <Scripts />
             </body>

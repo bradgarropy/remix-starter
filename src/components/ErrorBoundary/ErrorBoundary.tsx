18ad1d004540291a3ec15009f8456d8b9747cb37
Brad Garropy <bradgarropy@gmail.com>
2025-01-30 22:10:26 -0600

    make errors prettier.

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,32 +1,39 @@
 import {isRouteErrorResponse, useRouteError} from "@remix-run/react"
 
+import {createErrorStack} from "~/utils/errors"
+
 const ErrorBoundary = () => {
     const error = useRouteError()
 
     if (isRouteErrorResponse(error)) {
         return (
-            <>
-                <h1>
+            <div className="w-screen h-screen grid place-content-center p-16">
+                <h1 className="text-3xl font-bold mb-2">
                     {error.status} {error.statusText}
                 </h1>
 
-                <pre>{error.data}</pre>
-            </>
+                <pre className="overflow-auto">{error.data}</pre>
+            </div>
         )
     }
 
     if (error instanceof Error) {
         return (
-            <>
-                <h1>Error</h1>
-                <p>{error.message}</p>
-                <p>The stack trace is:</p>
-                <pre>{error.stack}</pre>
-            </>
+            <div className="w-screen h-screen grid place-content-center p-16">
+                <h1 className="text-3xl font-bold mb-2">
+                    Error: {error.message}
+                </h1>
+
+                <pre className="overflow-auto">{createErrorStack(error)}</pre>
+            </div>
         )
     }
 
-    return <h1>Unknown Error</h1>
+    return (
+        <div className="w-screen h-screen grid place-content-center p-16">
+            <h1 className="text-3xl font-bold mb-2">Unknown error</h1>
+        </div>
+    )
 }
 
 export default ErrorBoundary

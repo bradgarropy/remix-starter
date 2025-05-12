840814a0a3b209c7c02f7baa36d342fd6566d67b
Brad Garropy <bradgarropy@gmail.com>
2025-05-12 13:00:06 -0500

    add vercel support.

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,12 @@
 import type {Config} from "@react-router/dev/config"
+import {vercelPreset} from "@vercel/react-router/vite"
 
 const config = {
     appDirectory: "src",
     future: {
         unstable_optimizeDeps: true,
     },
+    presets: [vercelPreset()],
     serverModuleFormat: "esm",
     ssr: true,
 } satisfies Config

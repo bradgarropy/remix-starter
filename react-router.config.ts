e0e33734363376be71a65c6209d1b5203c96d4f6
Brad Garropy <bradgarropy@gmail.com>
2026-05-13 14:45:53 -0500

    react router scaffolding.

---
+++ react-router.config.ts
@@ -0,0 +1,14 @@
+import type {Config} from "@react-router/dev/config"
+
+const config = {
+    ssr: true,
+    appDirectory: "src",
+    future: {
+        v8_middleware: true,
+        v8_passThroughRequests: true,
+        v8_splitRouteModules: true,
+        v8_viteEnvironmentApi: true,
+    },
+} satisfies Config
+
+export default config
--- src/worker.ts
+++ src/worker.ts
@@ -1,35 +1,23 @@
-import {createRequestHandler, type ServerBuild} from "@remix-run/cloudflare"
+import {createRequestHandler} from "react-router"
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - build/server doesn't exist until after `remix vite:build`
-import * as build from "../build/server"
-import {getLoadContext} from "./loadContext"
+declare module "react-router" {
+    export interface AppLoadContext {
+        cloudflare: {
+            env: Env
+            ctx: ExecutionContext
+        }
+    }
+}
 
-const handleRemixRequest = createRequestHandler(build as unknown as ServerBuild)
+const requestHandler = createRequestHandler(
+    () => import("virtual:react-router/server-build"),
+    import.meta.env.MODE,
+)
 
 export default {
-    async fetch(request, env, ctx) {
-        try {
-            const loadContext = getLoadContext({
-                request,
-                context: {
-                    cloudflare: {
-                        cf: request.cf,
-                        ctx: {
-                            waitUntil: ctx.waitUntil.bind(ctx),
-                            passThroughOnException:
-                                ctx.passThroughOnException.bind(ctx),
-                            props: {},
-                        },
-                        caches,
-                        env,
-                    },
-                },
-            })
-            return await handleRemixRequest(request, loadContext)
-        } catch (error) {
-            console.error(error)
-            return new Response("An unexpected error occurred", {status: 500})
-        }
+    fetch(request, env, ctx) {
+        return requestHandler(request, {
+            cloudflare: {env, ctx},
+        })
     },
 } satisfies ExportedHandler<Env>

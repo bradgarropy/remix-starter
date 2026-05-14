b4a133275cc3bcb6d9f6859b3222c7d7aeee6587
Brad Garropy <bradgarropy@gmail.com>
2026-05-13 19:20:55 -0500

    fix typecheck.

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,6 @@ const config = {
     ssr: true,
     appDirectory: "src",
     future: {
-        v8_middleware: true,
         v8_passThroughRequests: true,
         v8_splitRouteModules: true,
         v8_viteEnvironmentApi: true,
